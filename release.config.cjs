7d1c4fcf01c6523aa339186c13d55f268d7e1513
cycjimmy <cycjimmy@gmail.com>
2022-03-26 21:34:00 +0800

    fix: fix root version after releasing

--- release.config.cjs
+++ release.config.cjs
@@ -1,38 +1,9 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+const makeConfig = require('@cycjimmy/config-lib/cjs/semanticRelease/19.x/makeConfigWithPgkRootForLibrary.cjs').default;
 const pkg = require('./package.json');
 
-module.exports = {
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'docs/CHANGELOG.md',
-      },
-    ],
-    ["@semantic-release/npm", {
-      "npmPublish": false
-    }],
-    ['@semantic-release/exec', {
-      prepareCmd: 'npm run package',
-    }],
-    ['@semantic-release/npm', {
-      pkgRoot: '.release',
-    }],
-    ['@semantic-release/github', {
-      assets: [pkg.browser],
-    }],
-    [
-      '@semantic-release/git',
-      {
-        assets: [
-          'docs/CHANGELOG.md',
-          'package.json',
-          'package-lock.json',
-        ],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ]
-  ],
-};
+module.exports = makeConfig({
+  githubOptions: {
+    assets: [pkg.browser],
+  },
+});
