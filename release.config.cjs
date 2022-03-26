0618c95f4ce79d22ec3d026608ae33e31f9ce8b2
cycjimmy <cycjimmy@gmail.com>
2022-03-26 21:10:43 +0800

    fix: test release

--- release.config.cjs
+++ release.config.cjs
@@ -1,9 +1,38 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-const makeCommon = require('@cycjimmy/config-lib/cjs/semanticRelease/19.x/makeConfigWithPgkRootForLibrary.cjs').default;
 const pkg = require('./package.json');
 
-module.exports = makeCommon({
-  githubOptions: {
-    assets: [pkg.browser],
-  },
-});
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'docs/CHANGELOG.md',
+      },
+    ],
+    ["@semantic-release/npm", {
+      "npmPublish": false
+    }],
+    ['@semantic-release/exec', {
+      prepareCmd: 'npm run package',
+    }],
+    ['@semantic-release/npm', {
+      pkgRoot: '.release',
+    }],
+    ['@semantic-release/github', {
+      assets: [pkg.browser],
+    }],
+    [
+      '@semantic-release/git',
+      {
+        assets: [
+          'docs/CHANGELOG.md',
+          'package.json',
+          'package-lock.json',
+        ],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ]
+  ],
+};
